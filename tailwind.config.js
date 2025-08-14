33c67ec8baf8edf4322668d06281c950e1f65d2e
Codruț Arzoiu <codrutarzoiu3@gmail.com>
2025-08-14 15:31:10 +0300

    taw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [content: ["./**/*.html"] ],
-   purge: {
-    enabled: true,
-    content: ['./**/*.html'],
-    options: {
-      safelist: ['text-red-500', 'bg-white', 'hover:bg-red-600'] // clase importante
-    }
-  },
-  
-  
-  
-  
-  
-  
+  content: ["./**/*.html"],  // toate fișierele HTML din proiect
   theme: {
     extend: {},
   },
